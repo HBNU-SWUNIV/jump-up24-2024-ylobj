65c7dec845f894d5b594df5c2f07746826a439ce
Kim Gihong <20221085@edu.hanbat.ac.kr>
2024-11-23 19:47:24 +0900

    도커 실행 스크립트 수정

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --detach -it --ipc=host --gpus '"device=0"' --name=gh-yolo-v8f \
+docker run --detach -it --ipc=host --gpus '"device=0"' \
  -v ./ultralytics:/ultralytics \
  -v ./datasets:/content/datasets \
  ultralytics/ultralytics:latest
